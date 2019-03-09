72cc72647ea49b18bd27c1fc8c0af635f7814398
c0r0n4 <coronateh@outlook.com>
2019-03-09 09:15:46 +0800

    completed the dumper with removing files from a cloned "body"

--- popup.js
+++ popup.js
@@ -10,45 +10,25 @@ function click(e) {
 // http://motyar.info/webscrapemaster/api/?url={url}&xpath={xpath}&attr={attr}&callback={callback}
 
 document.addEventListener('DOMContentLoaded', function() {
-    let uri = window.location.href;
-    let body = document.body.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
-    let scripts = document.querySelectorAll('script');
-    let inputs = document.querySelectorAll('input');
-    
-    document.body.remove(querySelectorAll('nav'));
-    let navs = document.querySelectorAll('nav');
-    navs.parentNode.removeChild(navs)
-
-    body.remove(inputs);
-    scripts.remove();
-    let json_output = JSON.stringify(body)
-    console.log(body);
-
-    $('nav').remove();
-    $('img').remove();
-    $('a').remove();
-    $('script').remove();
-    $('link').remove();
-    $('li').contents().unwrap();
-    $('li').remove();
-    $('body').remove('.');
-    $('body').removeClass("");
-})
-
-const images = {
-    joe: 'https://cloud.fullstackacademy.com/joe_alves.jpg',
-    kate: 'https://cloud.fullstackacademy.com/kate.png',
-    david: 'https://cloud.fullstackacademy.com/david_yang.png',
-    nimit: 'https://cloud.fullstackacademy.com/nimit_maru.jpg'
-}
-
-function help() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let body = JSON.parse(document.body);
-        console.log(body)
+    let body = document.body;
+    let cloned_body = body.cloneNode(true);
+    const deletes = [
+        'nav',
+        'header',
+        'footer',
+        'img',
+        'a',
+        'span',
+        'link',
+        'script',
+        'noscript'
+        // 'form'
+    ];
+    for (i in deletes) {
+        const item = cloned_body.querySelectorAll(deletes[i]);
+        [...item].forEach(e => e.remove());
     }
-    };
-    xmlhttp.send();
-}
+    cloned_body = cloned_body.textContent;
+    cloned_body = cloned_body.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+    cloned_body = cloned_body.replace(/<\!--.*?-->/g, "");
+})
