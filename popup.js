06c2ce581d4f4aeb918642327507956e154cae41
Claudia <claudialyx@gmail.com>
2019-03-18 15:55:16 +0800

    working popup in browser

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	let bank_list = [];
 	const selections = document.querySelectorAll('.sort-selection');
 	let selection;
-	document.addEventListener('click', function() {
+	document.addEventListener('click', function () {
 		console.log(selections)
 		for (select of selections) {
 			if (select.checked === true) {
@@ -42,33 +42,33 @@ document.addEventListener('DOMContentLoaded', function () {
 						populate(bank);
 					}
 				} else if (selection === 'type') {
-					
+
 				}
 			}
 		});
 
 	function populate(bank) {
 		const wrapper = document.getElementById("wrapper");
-			let bank_image = `./img/img_${bank.bank_name.toLowerCase()}.png`;
-			let bank_name = bank.bank_name;
-			let package_tag;
-			if (bank.package_tag === "personal") {
-				package_tag = "profile";
-			} else if (bank.package_tag === "islamic") {
-				package_tag = "bell";
-			} else if (bank.package_tag === "home") {
-				package_tag = "home3";
-			} else if (bank.package_tag === "car") {
-				package_tag = "truck";
-			} else if (bank.package_tag === "business") {
-				package_tag = "office";
-			}
-			let bank_package = bank.package_name;
-			let bank_rate = bank.interest_rate;
-			let bank_repayment = bank.repayment;
-			let bank_link = bank.link;
-			let card_bank =
-				`<div class="col-4 cardholder">
+		let bank_image = `./img/img_${bank.bank_name.toLowerCase()}.png`;
+		let bank_name = bank.bank_name;
+		let package_tag;
+		if (bank.package_tag === "personal") {
+			package_tag = "profile";
+		} else if (bank.package_tag === "islamic") {
+			package_tag = "bell";
+		} else if (bank.package_tag === "home") {
+			package_tag = "home3";
+		} else if (bank.package_tag === "car") {
+			package_tag = "truck";
+		} else if (bank.package_tag === "business") {
+			package_tag = "office";
+		}
+		let bank_package = bank.package_name;
+		let bank_rate = bank.interest_rate;
+		let bank_repayment = bank.repayment;
+		let bank_link = bank.link;
+		let card_bank =
+			`<div class="col-4 cardholder">
 					<a href="${bank_link}" target="_blank">
 						<div class="card mb-4">
 							<span title="${bank_name}">
@@ -84,16 +84,16 @@ document.addEventListener('DOMContentLoaded', function () {
 						</div>
 					</a>
 				</div>`;
-			wrapper.insertAdjacentHTML('afterbegin', card_bank);
+		wrapper.insertAdjacentHTML('afterbegin', card_bank);
 		// }
 	}
 	// This will execute in the current browser DOM
-	// chrome.tabs.executeScript(null, function () {
-	//     returned_matches = [];
-	//     web_content = filter_text();
-	//     send_and_fetch_details(web_content);
-	//     highlight_matches(returned_matches);
-	// })
+	chrome.tabs.executeScript(null, function () {
+		returned_matches = [];
+		web_content = filter_text();
+		send_and_fetch_details(web_content);
+		highlight_matches(returned_matches);
+	})
 });
 
 var InstantSearch = {
@@ -102,13 +102,13 @@ var InstantSearch = {
 		var internalHighlighter = function (options) {
 
 			var id = {
-					container: "container",
-					tokens: "tokens",
-					all: "all",
-					token: "token",
-					className: "className",
-					sensitiveSearch: "sensitiveSearch"
-				},
+				container: "container",
+				tokens: "tokens",
+				all: "all",
+				token: "token",
+				className: "className",
+				sensitiveSearch: "sensitiveSearch"
+			},
 				tokens = options[id.tokens],
 				allClassName = options[id.all][id.className],
 				allSensitiveSearch = options[id.all][id.sensitiveSearch];
@@ -194,7 +194,7 @@ var InstantSearch = {
 
 			iterator(options[id.container]);
 		} // End Function highlighter
-		;
+			;
 		internalHighlighter({
 			container: container,
 			all: {
@@ -219,14 +219,14 @@ function highlight_matches(highlightText) {
 function send_and_fetch_details(text_details) {
 	// 7. Send the data a specific website
 	fetch('http://127.0.0.1:5000/_words', {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				data: text_details
-			})
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			data: text_details
 		})
+	})
 		// Check the response of fetching "data" after submitting to the specified website
 		.then(checkStatus)
 		// Perform change of received "data" structure obtained from website
@@ -276,152 +276,3 @@ function filter_text() {
 	return stringified_body;
 }
 
-const injected_css = `
-<style type="text/css" media="screen">
-#popout,
-#toggle {
-	position: fixed;
-	transition: all 2s;
-}
-
-#info {
-	z-index: 1000;
-}
-
-#toggle {
-	bottom: 1em;
-	right: 1em;
-	display: grid;
-	grid-template-columns: minmax(min-content, 10em) minmax(min-content, 3em);
-	grid-template-areas: "detail";
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background-color: rgb(10, 177, 19);
-	z-index: 500;
-	border-bottom-left-radius: 0.7em;
-	border-top-left-radius: 0.7em;
-}
-
-#toggle:hover {
-	background-color: rgb(255, 85, 85);
-}
-
-#toggle:hover .highlight {
-	font-size: 1.15em;
-	color: rgb(194, 197, 199);
-}
-
-#toggle * {
-	transition: all 0.3s;
-	vertical-align: middle;
-	padding: 0.2em;
-	color: white;
-}
-
-.detail {
-	grid-area: detail;
-}
-
-#info {
-	position: fixed;
-	border: 0.1em solid black;
-	padding: 0.3em;
-	width: 1em;
-	height: 1em;
-	bottom: 3em;
-	right: 0;
-	background-color: #4ebbb5;
-	color: white;
-	font-size: 1.2rem;
-	border-radius: 50%;
-	text-align: center;
-	box-shadow: 0 0 10px #719ECE;
-	transition-property: transform;
-	transition-duration: 1s;
-}
-
-#info:hover {
-	cursor: pointer;
-	background-color: #105372;
-	border: 1px solid #2E3A46;
-	animation-name: rotate; 
-	animation-duration: 2s; 
-	animation-iteration-count: infinite;
-	animation-timing-function: linear;
-}
-
-@keyframes rotate {
-	from {transform: rotate(0deg);}
-	to {transform: rotate(360deg);}
-}
-
-input[type=checkbox]#popout {
-	display: none;
-}
-#popout ~ #toggle {
-	margin-right: -100%;
-}
-
-.slider:hover #popout ~ #toggle {
-	margin-right: 0;
-}
-
-.slider .clear * {
-	text-decoration: initial;
-	color: initial;
-}
-
-.flash {
-	z-index: 1500;
-	position: fixed;
-	bottom: 3em;
-	right: 0;
-	background-color: tomato;
-	box-shadow: 0 5px 10px #ddd;
-	border: 2px solid violet;
-	padding: 0.1em 0.5em;
-	border-top-left-radius: 1.25em;
-	border-bottom-left-radius: 1.25em;
-	animation: disappear 3s;
-	animation-fill-mode: forwards;
-}
-
-.flash * {
-	transition-delay: 3s;
-	
-}
-
-@keyframes disappear {
-	0% { margin-right: 10%; }
-	15% { margin-right: 7% }
-	30% { margin-right: 4%; }
-	45% { margin-right: 1%; }
-	60% { margin-right: 0%; }
-	100% { margin-right: -100%; }
-}
-</style>
-`;
-
-// const first_bank = bank_list[0];
-// let injected_section = `
-// <div class="slider">
-// 	<a href="${first_bank.bank_link}" target="_blank" class="clear">
-// 		<input type="checkbox" name="popout" id="popout">
-// 		<label for="popout" id="info">&times;</label>
-// 		<div id="toggle">
-// 			<div class="detail">
-// 				<div class="highlight"><span class="icon icon-${first_bank.package_tag}"></span>${first_bank.bank_name}</div>
-// 				<div>${first_bank.bank_rate} @ RM ${first_bank.bank_repayment}</div>
-// 			</div>
-// 		</div>
-// 	</a>
-// </div>
-
-// <div class="flash">
-// 	<p>
-// 		<span class="icon icon-${first_bank.package_tag}"></span>
-// 		${first_bank.bank_name} offers ${first_bank.bank_rate} @ RM ${first_bank.bank_repayment}</div>
-// 	</p>
-// </div>
-// `;
