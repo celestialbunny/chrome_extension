9860bc11b8863437ca4830a5b1214de20836da07
Chai Chong Teh <coronateh@outlook.com>
2019-03-18 21:05:02 +0800

    updated code so far

--- popup.js
+++ popup.js
@@ -30,13 +30,13 @@ document.addEventListener('DOMContentLoaded', function () {
 			// Original full collection
 			bank_list = data;
 			// Sorting using bank_name
-			order_by_bank_name = _.sortBy(bank_list, 'bank_name')
+			order_by_bank_name = _.sortBy(bank_list, 'bank_name').reverse();
 			// Sorting using package_tag
 			order_by_package_tag = _.sortBy(bank_list, 'package_tag');
 			// Sorting using interest rate
-			order_by_interest_rate = _.sortBy(bank_list, 'interest_rate');
+			order_by_interest_rate = _.sortBy(bank_list, 'interest_rate').reverse();
 			// Sorting using repayment
-			order_by_repayment = _.sortBy(bank_list, 'repayment');
+			order_by_repayment = _.sortBy(bank_list, 'repayment').reverse();
 			/**
 			 * Reveal the data fetched from API
 			 */
@@ -130,152 +130,14 @@ document.addEventListener('DOMContentLoaded', function () {
 				// }
 			}
 			// This will execute in the current browser DOM
-			chrome.tabs.executeScript(null, function () {
-				returned_matches = [];
-				web_content = filter_text();
-				send_and_fetch_details(web_content);
-				highlight_matches(returned_matches);
-			})
+			// chrome.tabs.executeScript(null, function () {
+			// 	// highlight_matches(returned_matches);
+			// })
 		});
 
-	var InstantSearch = {
-
-		"highlight": function (container, highlightText) {
-			var internalHighlighter = function (options) {
-
-				var id = {
-					container: "container",
-					tokens: "tokens",
-					all: "all",
-					token: "token",
-					className: "className",
-					sensitiveSearch: "sensitiveSearch"
-				},
-					tokens = options[id.tokens],
-					allClassName = options[id.all][id.className],
-					allSensitiveSearch = options[id.all][id.sensitiveSearch];
 
 
-				function checkAndReplace(node, tokenArr, classNameAll, sensitiveSearchAll) {
-					var nodeVal = node.nodeValue,
-						parentNode = node.parentNode,
-						i, j, curToken, myToken, myClassName, mySensitiveSearch,
-						finalClassName, finalSensitiveSearch,
-						foundIndex, begin, matched, end,
-						textNode, span, isFirst;
 
-					for (i = 0, j = tokenArr.length; i < j; i++) {
-						curToken = tokenArr[i];
-						myToken = curToken[id.token];
-						myClassName = curToken[id.className];
-						mySensitiveSearch = curToken[id.sensitiveSearch];
-
-						finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
-
-						finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
-
-						isFirst = true;
-						while (true) {
-							if (finalSensitiveSearch)
-								foundIndex = nodeVal.indexOf(myToken);
-							else
-								foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
-
-							if (foundIndex < 0) {
-								if (isFirst)
-									break;
-
-								if (nodeVal) {
-									textNode = document.createTextNode(nodeVal);
-									parentNode.insertBefore(textNode, node);
-								} // End if (nodeVal)
-
-								parentNode.removeChild(node);
-								break;
-							} // End if (foundIndex < 0)
-
-							isFirst = false;
-
-
-							begin = nodeVal.substring(0, foundIndex);
-							matched = nodeVal.substr(foundIndex, myToken.length);
-
-							if (begin) {
-								textNode = document.createTextNode(begin);
-								parentNode.insertBefore(textNode, node);
-							} // End if (begin)
-
-							span = document.createElement("span");
-							span.className += finalClassName;
-							span.appendChild(document.createTextNode(matched));
-							parentNode.insertBefore(span, node);
-
-							nodeVal = nodeVal.substring(foundIndex + myToken.length);
-						} // Whend
-
-					} // Next i 
-				}; // End Function checkAndReplace 
-
-				function iterator(p) {
-					if (p === null) return;
-
-					var children = Array.prototype.slice.call(p.childNodes),
-						i, cur;
-
-					if (children.length) {
-						for (i = 0; i < children.length; i++) {
-							cur = children[i];
-							if (cur.nodeType === 3) {
-								checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
-							} else if (cur.nodeType === 1) {
-								iterator(cur);
-							}
-						}
-					}
-				}; // End Function iterator
-
-				iterator(options[id.container]);
-			} // End Function highlighter
-				;
-			internalHighlighter({
-				container: container,
-				all: {
-					className: "highlighter"
-				},
-				tokens: [{
-					token: highlightText,
-					className: "highlight",
-					sensitiveSearch: false
-				}]
-			}); // End Call internalHighlighter 
-
-		} // End Function highlight
-
-	};
-
-	function highlight_matches(highlightText) {
-		const body = document.body;
-		InstantSearch.highlight(body, highlightText);
-	}
-
-	function send_and_fetch_details(text_details) {
-		// 7. Send the data a specific website
-		fetch('http://127.0.0.1:5000/_words', {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				data: text_details
-			})
-		})
-			// Check the response of fetching "data" after submitting to the specified website
-			.then(checkStatus)
-			// Perform change of received "data" structure obtained from website
-			.then(res => res.json())
-			// Perform operation on the "altered" data format
-			.then(data => returned_matches.push(data))
-	}
 
 	function checkStatus(response) {
 		if (response.ok) {
@@ -285,36 +147,5 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
-	function filter_text() {
-		// 1. Select the portion of the document to be processed
-		let body = document.body;
-		// 2. Make a copy of the portion and perform operation from here in order not to affect the "current" webpage
-		let cloned_body = body.cloneNode(true);
-		// 3. select the " undesired parts" to be dumped
-		const deletes = [
-			'nav',
-			'header',
-			'footer',
-			'img',
-			'a',
-			'span',
-			'link',
-			'script',
-			'noscript'
-		];
-		// 4. Perform the deletion of parts via destructuring each individual selected "portion"
-		for (i in deletes) {
-			const item = cloned_body.querySelectorAll(deletes[i]);
-			[...item].forEach(e => e.remove());
-		}
-		// 5. Select the partially "cleansed" data to be "purified"
-		cloned_body = cloned_body.textContent;
-		cloned_body = cloned_body.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
-		cloned_body = cloned_body.replace(/<\!--.*?-->/g, "");
-		cloned_body = cloned_body.toLowerCase();
-		// 6. Perform suitable "formatting" to convert "cleansed" data into JSON
-		// let parsed_body = JSON.parse(stringified_body);
-		let stringified_body = JSON.stringify(cloned_body);
-		return stringified_body;
-	}
-
+	
+});
