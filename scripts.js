56ba5f4ce2a965dcc499a57d4710e5b46c0e79c0
Claudia <claudialyx@gmail.com>
2019-03-19 10:26:19 +0800

    amended some css

--- scripts.js
+++ scripts.js
@@ -11,26 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     let injected_section = `
-    <div class="slider">
-        <a href="${first_bank.bank_link}" target="_blank" class="clear">
-            <input type="checkbox" name="popout" id="popout">
-            <label for="popout" id="info">&times;</label>
-            <div id="toggle">
-                <div class="detail">
-                    <div class="highlight"><span class="icon icon-${first_bank.package_tag}"></span>${first_bank.bank_name}</div>
-                    <div>${first_bank.interest_rate} @ RM ${first_bank.repayment}</div>
-                </div>
-            </div>
-        </a>
-    </div>
-    
     <div class="flash">
-        <p>
-            <span class="icon icon-${first_bank.package_tag}"></span>
-            ${first_bank.bank_name} offers ${first_bank.interest_rate} @ RM ${first_bank.repayment}</div>
-        </p>
+    <p>
+    <span class="icon icon-${first_bank.package_tag}"></span>
+    <span>Hey! 👋 ${first_bank.package_name} is currently offering ${first_bank.package_tag} loan at ${first_bank.interest_rate}% ! Click me to find out more! </span>
+    </p>
     </div>
     `;
+    // <div class="slider">
+    //     <a href="${first_bank.bank_link}" target="_blank" class="clear">
+    //         <input type="checkbox" name="popout" id="popout">
+    //         <label for="popout" id="info">&times;</label>
+    //         <div id="toggle">
+    //             <div class="detail">
+    //                 <div class="highlight"><span class="icon icon-${first_bank.package_tag}"></span>${first_bank.bank_name}</div>
+    //                 <span>Hey! 👋 ${first_bank.package_name} is currently offering ${first_bank.package_tag} loan at ${first_bank.interest_rate}% ! Click me to find out more! </span>
+    //             </div>
+    //         </div>
+    //     </a>
+    // </div>
 
     document.body.insertAdjacentHTML("beforeend", injected_section)
 })
